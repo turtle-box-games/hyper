10bd3e2d6c182dc48615462a4a1352e9ea398c12
Michael Miller <icy.arctic.fox@gmail.com>
2017-12-30 16:38:14 -0700

    Add remaining tests for byte type

--- test/ByteTest.cpp
+++ test/ByteTest.cpp
@@ -73,6 +73,116 @@ TEST(byte, RightShiftZeroRef) {
     EXPECT_EQ(byte(0), b);
 }
 
+TEST(byte, Or) {
+    EXPECT_EQ(byte(186), byte(42) | byte(146));
+}
+
+TEST(byte, OrZero) {
+    EXPECT_EQ(byte(42), byte(42) | byte(0));
+}
+
+TEST(byte, OrMax) {
+    EXPECT_EQ(byte(255), byte(42) | byte(255));
+}
+
+TEST(byte, OrRef) {
+    byte b = byte(42);
+    b |= byte(146);
+    EXPECT_EQ(byte(186), b);
+}
+
+TEST(byte, OrZeroRef) {
+    byte b = byte(42);
+    b |= byte(0);
+    EXPECT_EQ(byte(42), b);
+}
+
+TEST(byte, OrMaxRef) {
+    byte b = byte(42);
+    b |= byte(255);
+    EXPECT_EQ(byte(255), b);
+}
+
+TEST(byte, And) {
+    EXPECT_EQ(byte(2), byte(42) & byte(146));
+}
+
+TEST(byte, AndZero) {
+    EXPECT_EQ(byte(0), byte(42) & byte(0));
+}
+
+TEST(byte, AndMax) {
+    EXPECT_EQ(byte(42), byte(42) & byte(255));
+}
+
+TEST(byte, AndRef) {
+    byte b = byte(42);
+    b &= byte(146);
+    EXPECT_EQ(byte(2), b);
+}
+
+TEST(byte, AndZeroRef) {
+    byte b = byte(42);
+    b &= byte(0);
+    EXPECT_EQ(byte(0), b);
+}
+
+TEST(byte, AndMaxRef) {
+    byte b = byte(42);
+    b &= byte(255);
+    EXPECT_EQ(byte(42), b);
+}
+
+TEST(byte, Xor) {
+    EXPECT_EQ(byte(184), byte(42) ^ byte(146));
+}
+
+TEST(byte, XorZero) {
+    EXPECT_EQ(byte(42), byte(42) ^ byte(0));
+}
+
+TEST(byte, XorMax) {
+    EXPECT_EQ(byte(213), byte(42) ^ byte(255));
+}
+
+TEST(byte, XorRef) {
+    byte b = byte(42);
+    b ^= byte(146);
+    EXPECT_EQ(byte(184), b);
+}
+
+TEST(byte, XorZeroRef) {
+    byte b = byte(42);
+    b ^= byte(0);
+    EXPECT_EQ(byte(42), b);
+}
+
+TEST(byte, XorMaxRef) {
+    byte b = byte(42);
+    b ^= byte(255);
+    EXPECT_EQ(byte(213), b);
+}
+
+TEST(byte, Not) {
+    EXPECT_EQ(byte(213), ~byte(42));
+}
+
+TEST(byte, NotZero) {
+    EXPECT_EQ(byte(255), ~byte(0));
+}
+
+TEST(byte, NotMax) {
+    EXPECT_EQ(byte(0), ~byte(255));
+}
+
 TEST(byte, ToInt) {
     EXPECT_EQ(42, toInt(byte(42)));
+}
+
+TEST(byte, ToIntZero) {
+    EXPECT_EQ(0, toInt(byte(0)));
+}
+
+TEST(byte, ToIntMax) {
+    EXPECT_EQ(255, toInt(byte(255)));
 }
