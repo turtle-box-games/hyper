856ec5c52af1929184079817c285c8a6bba5b651
Michael Miller <icy.arctic.fox@gmail.com>
2018-01-28 23:25:16 -0700

    Implement stack trace snapshot functionality

--- include/hyper/StackTraceSnapshot.h
+++ include/hyper/StackTraceSnapshot.h
@@ -5,6 +5,7 @@
 #define HYPER_STACKTRACESNAPSHOT_H
 
 #include <cstddef> // For size_t.
+#include <execinfo.h> // For backtrace().
 
 namespace hyper
 {
@@ -18,21 +19,38 @@ namespace hyper
     template<size_t Size = 64>
     class StackTraceSnapshot
     {
-    private:
-        size_t _frameCount;
-        void *_addresses[Size];
-
     public:
+        /// @brief Short-hand for addresses.
+        /// @details Fixed array of raw pointers.
+        typedef void *AddressArray[Size];
+
+        /// @brief Default constructor.
+        /// @details Creates a snapshot of the current stack state.
+        StackTraceSnapshot() noexcept
+        {
+            _frameCount = backtrace(_addresses, Size);
+        }
+
         /// @brief Frame count.
         /// @details Number of frame contained in the snapshot.
         /// @return Number of frames.
-        size_t frameCount() const noexcept;
+        size_t frameCount() const noexcept
+        {
+            return _frameCount;
+        }
 
         /// @brief Return addresses.
         /// @details Raw return addresses captured from the stack.
         ///   These addresses refer to points where function calls return to.
         ///   This information can be traced back to the calling functions.
-        const void **addresses() const noexcept;
+        AddressArray const &addresses() const noexcept
+        {
+            return _addresses;
+        }
+
+    private:
+        size_t _frameCount;
+        AddressArray _addresses;
     };
 }
 
