284eee26aeaf94c8a1f5c50a4d8c9909ffb3fe6a
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-13 21:16:08 -0700

    Try out shared pointer usage

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,20 @@
+#include <hyper/SharedPointer.h>
+
+using namespace hyper;
+
 int main(int argc, char **argv)
 {
+    auto p = new int;
+    SharedPointer<int> sp(p);
+    {
+        *p = 5;
+        printf("%i\n", *sp);
+        SharedPointer<int> ptr1(sp);
+        SharedPointer<int> ptr2(ptr1);
+        SharedPointer<int> ptr3(ptr2);
+
+        printf("%i + %i + %i = %i\n", *ptr1, *ptr2, *ptr3, *ptr1 + *ptr2 + *ptr3);
+    }
+
     return 0;
 }
