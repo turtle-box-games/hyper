69d90dffcfab5e29b6277bd9602aff09fd370173
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-16 15:59:58 -0700

    Update to use new smart pointer constructors
    Also try out swap utility.

--- test/test.cpp
+++ test/test.cpp
@@ -4,16 +4,15 @@ using namespace hyper;
 
 int main(int argc, char **argv)
 {
-    auto p = new int;
-    SharedPointer<int> sp(p);
+    SharedPointer<int> sp(new int(5));
     {
-        *p = 5;
         printf("%i\n", *sp);
         SharedPointer<int> ptr1(sp);
         SharedPointer<int> ptr2(ptr1);
         SharedPointer<int> ptr3(ptr2);
 
         printf("%i + %i + %i = %i\n", *ptr1, *ptr2, *ptr3, *ptr1 + *ptr2 + *ptr3);
+        swap(ptr1, ptr2);
     }
 
     return 0;
