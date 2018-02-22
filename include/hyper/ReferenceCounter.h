56131b13952daed03ed1eb2ae2f4f919c9f2ecc2
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-22 00:45:13 -0700

    Fix issues with naming and docs

--- include/hyper/ReferenceCounter.h
+++ include/hyper/ReferenceCounter.h
@@ -1,4 +1,4 @@
-/// @param ReferenceCounter.h
+/// @file ReferenceCounter.h
 /// Underlying implementation for tracking references.
 
 #ifndef HYPER_REFERENCECOUNTER_H
@@ -9,7 +9,7 @@
 
 namespace hyper
 {
-    /// @brief Underlying implementation for tracking references.
+    /// @brief Internal implementation for tracking references in smart pointers.
     /// @details All smart pointers that refer to the same instance share a single instance of this class.
     ///   This allows the smart pointers to "communicate" when they are no longer being used.
     /// @tparam T Type of instance to reference.
@@ -20,25 +20,25 @@ namespace hyper
     {
     private:
         size_t _count;
-        T *_arr;
+        T *_ptr;
 
     public:
         /// @brief Default constructor.
         /// @details Creates a new reference counter.
-        /// @param arr Array to count references to.
-        constexpr ReferenceCounter(T *arr) noexcept
-                : _count(1), _arr(arr)
+        /// @param ptr Pointer to count references to.
+        constexpr ReferenceCounter(T *ptr) noexcept
+                : _count(1), _ptr(ptr)
         {
             // ...
         }
 
         /// @brief Destructor.
-        /// @details Resets the count to zero and releases the memory held by the array, if any.
+        /// @details Resets the count to zero and releases the memory held by the pointer, if any.
         ~ReferenceCounter()
         {
             _count = 0;
             Destructor destructor;
-            destructor(_arr);
+            destructor(_ptr);
         }
 
         /// @brief Increments the reference count by one.
@@ -48,19 +48,19 @@ namespace hyper
         }
 
         /// @brief Decrements the reference count by one.
-        /// @return True if there are still references to the array.
-        /// @return False if the last reference was just released and the array can be freed.
+        /// @return True if there are still references to the pointer.
+        /// @return False if the last reference was just released and the pointer can be freed.
         bool decrement() noexcept
         {
             ASSERTF(_count > 0, "Attempted to decrement reference count below zero");
             return 0 < --_count;
         }
 
-        /// @brief Retrieves the raw array.
-        /// @return Array being counted.
+        /// @brief Retrieves the raw pointer.
+        /// @return Pointer being counted.
         constexpr T *getReference() const noexcept
         {
-            return _arr;
+            return _ptr;
         }
     };
 }
