85bb4bf398683a4830db38d4ec6a5f718338a3eb
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-21 20:30:23 -0700

    Change SharedArray to a specialization of SharedPointer

--- test/SharedPointerTest.cpp
+++ test/SharedPointerTest.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 #include "hyper/SharedPointer.h"
 
+#define SAMPLE_ARRAY_SIZE 100
+#define SAMPLE_VALUE 42
+
 using namespace hyper;
 
 class SharedPointerDestructorCapture
@@ -29,6 +32,41 @@ struct SharedPointerSampleData
 
     SharedPointerSampleData(int val)
             : value(val)
+    {
+        // ...
+    }
+};
+
+class SharedArrayDestructorCapture
+{
+private:
+    int *_destructorCallCount;
+
+public:
+    SharedArrayDestructorCapture()
+            : _destructorCallCount(nullptr)
+    {
+        // ...
+    }
+
+    SharedArrayDestructorCapture(int *count)
+            : _destructorCallCount(count)
+    {
+        // ...
+    }
+
+    ~SharedArrayDestructorCapture()
+    {
+        (*_destructorCallCount)++;
+    }
+};
+
+struct SharedArraySampleValue
+{
+    int value;
+
+    SharedArraySampleValue()
+            : value(SAMPLE_VALUE)
     {
         // ...
     }
@@ -90,4 +128,53 @@ TEST(SharedPointer, BoolCastTrue) {
 TEST(SharedPointer, BoolCastFalse) {
     SharedPointer<int> sp(nullptr);
     EXPECT_FALSE((bool)sp);
+}
+
+TEST(SharedPointer, ArrayFree) {
+    int freeCount = -SAMPLE_ARRAY_SIZE; // Negative because copying instances triggers destructor.
+    {
+        SharedPointer<SharedArrayDestructorCapture[]> sa(new SharedArrayDestructorCapture[SAMPLE_ARRAY_SIZE]);
+        // freeCount == 0 here.
+        for(size_t i = 0; i < SAMPLE_ARRAY_SIZE; ++i)
+            sa[i] = SharedArrayDestructorCapture(&freeCount); // Copy occurs, this is why freeCount starts as negative.
+        // sa should be freed when this scope exits,
+        // which will increment freeCount for every destructor called.
+    }
+    EXPECT_EQ(freeCount, SAMPLE_ARRAY_SIZE);
+}
+
+TEST(SharedPointer, ArraySwap) {
+    const auto val1 = 42;
+    const auto val2 = 24;
+    SharedPointer<int[]> sa1(new int[SAMPLE_ARRAY_SIZE]);
+    SharedPointer<int[]> sa2(new int[SAMPLE_ARRAY_SIZE]);
+    sa1[0] = val1;
+    sa2[0] = val2;
+    sa1.swap(sa2);
+    EXPECT_EQ(sa1[0], val2);
+    EXPECT_EQ(sa2[0], val1);
+}
+
+TEST(SharedPointer, SubscriptGet) {
+    SharedPointer<SharedArraySampleValue[]> sa(SAMPLE_ARRAY_SIZE);
+    for(size_t i = 0; i < SAMPLE_ARRAY_SIZE; ++i)
+        EXPECT_EQ(sa[i].value, SAMPLE_VALUE);
+}
+
+TEST(SharedPointer, SubscriptSet) {
+    SharedPointer<size_t[]> sa(SAMPLE_ARRAY_SIZE);
+    for(size_t i = 0; i < SAMPLE_ARRAY_SIZE; ++i) {
+        sa[i] = i;
+        EXPECT_EQ(sa[i], i);
+    }
+}
+
+TEST(SharedPointer, ArrayBoolCastTrue) {
+    SharedPointer<int[]> sa(new int[SAMPLE_ARRAY_SIZE]);
+    EXPECT_TRUE((bool)sa);
+}
+
+TEST(SharedPointer, ArrayBoolCastFalse) {
+    SharedPointer<int[]> sa(nullptr);
+    EXPECT_FALSE((bool)sa);
 }
