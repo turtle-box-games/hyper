4f07d3c0d0b1d2598834abe4a11c36581b944a98
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-24 19:19:54 -0700

    Remove unnecessary contents of else() and endif()

--- cmake/SetCompilerWarningAll.cmake
+++ cmake/SetCompilerWarningAll.cmake
@@ -6,27 +6,27 @@ if(CMAKE_BUILD_TOOL MATCHES "(msdev|devenv|nmake)")
     set(CMAKE_CXX_WARNING_LEVEL 4)
     if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
         string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-    else(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+    else()
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
-    endif(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+    endif()
 
     set(CMAKE_C_WARNING_LEVEL 4)
     if(CMAKE_C_FLAGS MATCHES "/W[0-4]")
         string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
-    else(CMAKE_C_FLAGS MATCHES "/W[0-4]")
+    else()
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W4")
-    endif(CMAKE_C_FLAGS MATCHES "/W[0-4]")
+    endif()
 
 elseif(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC
         OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     # use -Wall for gcc/clang.
     if(NOT CMAKE_CXX_FLAGS MATCHES "-Wall")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Weffc++")
-    endif(NOT CMAKE_CXX_FLAGS MATCHES "-Wall")
+    endif()
 
     if(NOT CMAKE_C_FLAGS MATCHES "-Wall")
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Weffc++")
-    endif(NOT CMAKE_C_FLAGS MATCHES "-Wall")
+    endif()
 
 else()
     message(STATUS "Unknown compiler, cannot set warning flags")
