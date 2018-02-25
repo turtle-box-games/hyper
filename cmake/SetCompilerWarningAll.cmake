7d1a76ed018ef72c2d898c429db460aae7033330
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-24 19:16:26 -0700

    Add -Wextra and -Weffc++ flags

--- cmake/SetCompilerWarningAll.cmake
+++ cmake/SetCompilerWarningAll.cmake
@@ -20,13 +20,13 @@ if(CMAKE_BUILD_TOOL MATCHES "(msdev|devenv|nmake)")
 elseif(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUC
         OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     # use -Wall for gcc/clang.
-    if(NOT CMAKE_CXX_FLAGS MATCHES "-Wall$")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
-    endif(NOT CMAKE_CXX_FLAGS MATCHES "-Wall$")
+    if(NOT CMAKE_CXX_FLAGS MATCHES "-Wall")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Weffc++")
+    endif(NOT CMAKE_CXX_FLAGS MATCHES "-Wall")
 
-    if(NOT CMAKE_C_FLAGS MATCHES "-Wall$")
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
-    endif(NOT CMAKE_C_FLAGS MATCHES "-Wall$")
+    if(NOT CMAKE_C_FLAGS MATCHES "-Wall")
+        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Weffc++")
+    endif(NOT CMAKE_C_FLAGS MATCHES "-Wall")
 
 else()
     message(STATUS "Unknown compiler, cannot set warning flags")
