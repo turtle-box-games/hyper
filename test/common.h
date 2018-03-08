f8fd3eff43dc7aa418e1a4fb46ca2d0a2a472794
Michael Miller <icy.arctic.fox@gmail.com>
2018-03-07 20:04:35 -0700

    Add common header for testing
    Define TEST_DESCRIPTION() to document test purpose.

---
+++ test/common.h
@@ -0,0 +1,8 @@
+#ifndef HYPER_TEST_COMMON_H
+#define HYPER_TEST_COMMON_H
+
+#include "gmock/gmock.h"
+
+#define TEST_DESCRIPTION(description) RecordProperty("description", description)
+
+#endif // HYPER_TEST_COMMON_H
