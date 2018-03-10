0441256a44b2cf6b84b95afa1cae6c74d97a31fe
Michael Miller <icy.arctic.fox@gmail.com>
2018-03-09 21:37:49 -0700

    Testing function class

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,11 @@
-#include <hyper/SharedPointer.h>
+#include <hyper/Function.h>
 
 using namespace hyper;
 
 int main(int argc, char **argv) {
-    SharedPointer<int> sp(new int(5));
-    {
-        printf("%i\n", *sp);
-        SharedPointer<int> ptr1(sp);
-        SharedPointer<int> ptr2(ptr1);
-        SharedPointer<int> ptr3(ptr2);
-
-        printf("%i + %i + %i = %i\n", *ptr1, *ptr2, *ptr3, *ptr1 + *ptr2 + *ptr3);
-        swap(ptr1, ptr2);
-    }
+    Function<void(void)> fun;
+    fun = []() { printf("fun\n"); };
+    fun();
 
     return 0;
 }
