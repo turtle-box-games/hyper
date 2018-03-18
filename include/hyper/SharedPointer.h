d027f1158a91cd5cc31f9b420b0da9a0785fc06f
Michael Miller <icy.arctic.fox@gmail.com>
2018-03-17 21:56:00 -0600

    Mimic unique pointer, but without the subtypes (for now)

--- include/hyper/SharedPointer.h
+++ include/hyper/SharedPointer.h
@@ -1,84 +1,89 @@
 /// @file SharedPointer.h
-/// Definition of a smart pointer that can be shared throughout the application
-/// and it will automatically be freed when it is no longer referenced.
+/// Type of smart pointer that tracks its references and automatically frees itself when it's no longer used.
 
 #ifndef HYPER_SHARED_POINTER_H
 #define HYPER_SHARED_POINTER_H
 
 #include "assert.h"
+#include "DefaultDeleter.h"
+#include "utility.h"
 #include "ReferenceCounter.h"
 
 namespace hyper {
-    /// @brief Smart pointer that can be shared throughout the application.
-    /// @details Smart pointer that tracks its references.
-    ///   The pointer will automatically be destroyed (and resources freed)
-    ///   when there are no more references to it.
-    ///   Instances of this class should have the only references to a raw pointer.
-    ///   This class is designed in such a way to attempt to prevent external references.
+    /// @brief Smart pointer that allows multiple references to a single instance.
+    /// @details Smart pointer that holds a shared reference.
+    ///   References to the pointer are tracked,
+    ///   and when there aren't any references remaining, the pointer will be freed.
     /// @tparam T Type the pointer references.
     template<typename T>
     class SharedPointer {
-    private:
-        ReferenceCounter<T> *_impl;
-
     public:
         /// @brief Default constructor.
-        /// @details Creates a new shared pointer referencing null.
-        constexpr explicit SharedPointer() noexcept
+        /// @details Creates a new shared pointer that references null.
+        constexpr SharedPointer() noexcept
                 : _impl(new ReferenceCounter<T>(nullptr)) {
             // ...
         }
 
         /// @brief General constructor.
-        /// @details Creates a new shared pointer with an existing reference.
-        /// @param ptr Raw pointer to wrap.
-        constexpr explicit SharedPointer(T *&&ptr) noexcept
-                : _impl(new ReferenceCounter<T>(ptr)) {
+        /// @details Creates a new shared pointer that wraps an existing raw pointer.
+        /// @param rawPointer Raw pointer to wrap.
+        constexpr explicit SharedPointer(T *&&rawPointer) noexcept
+                : _impl(new ReferenceCounter<T>(rawPointer)) {
             // ...
         }
 
         /// @brief Copy constructor.
-        /// @details Creates a new shared pointer that references an existing one.
-        /// @param other Existing shared pointer.
+        /// @details Shares a reference to an existing pointer.
+        /// @param other Existing pointer to reference.
         SharedPointer(const SharedPointer &other) noexcept
                 : _impl(other._impl) {
             _impl->increment();
         }
 
-        /// @brief Move constructor.
-        /// @details Creates a new shared pointer from a temporary one.
-        /// @param other Temporary shared pointer.
-        SharedPointer(SharedPointer &&other) noexcept
-                : _impl(other._impl) {
-            other._impl = nullptr;
+        /// @brief Destructor.
+        /// @details Releases a reference to the underlying pointer.
+        ///   If there are no more references, then the pointer and any resources it holds are released.
+        ~SharedPointer() {
+            expire();
         }
 
-        /// @brief Destructor.
-        /// @details Releases the resources referenced by the shared pointer.
-        ///   The underlying pointer will not be released unless all other references have been released.
-        ~SharedPointer() noexcept {
-            if (_impl != nullptr) {
-                // Release reference counter if this instance is the last one using it.
-                if (!_impl->decrement())
+        /// @brief Force expiration of this reference.
+        /// @details Has the same effect as letting the shared pointer go out of scope.
+        ///   If there are no more references, then the pointer and any resources it holds are released.
+        void expire() noexcept {
+            if(_impl != nullptr) {
+                if(!_impl->decrement())
                     delete _impl;
                 _impl = nullptr;
             }
         }
 
-        /// @brief Swaps the contents of two Shared pointers.
-        /// @param other Shared pointer to swap with.
+        /// @brief Re-initializes the shared pointer.
+        /// @details Changes the raw pointer that the instance wraps.
+        ///   The existing pointer, if it has no other references, is freed.
+        /// @param rawPointer New pointer to wrap.
+        void reset(T *&&rawPointer = nullptr) noexcept {
+            expire();
+            _impl = new ReferenceCounter<T>(rawPointer);
+        }
+
+        /// @brief Swaps with another instance.
+        /// @details Exchanges the underlying pointer between two instances of the same type.
+        /// @param other Other pointer to swap with.
         void swap(SharedPointer &other) noexcept {
             auto temp = other._impl;
             other._impl = _impl;
             _impl = temp;
         }
 
-        /// @brief Member access operator.
+        /// @brief Indirect access operator.
         /// @details Provides access to the underlying reference.
         /// @return Underlying reference.
         /// @note Be sure that it is safe to de-reference the pointer.
         ///   The pointer is asserted to be non-null.
         constexpr T &operator*() const noexcept {
+            ASSERTF(_impl != nullptr, "Attempt to use expired pointer");
             auto ptr = _impl->getPointer();
             ASSERTF(ptr != nullptr, "Attempt to dereference null pointer");
             return *ptr;
@@ -90,6 +95,7 @@ namespace hyper {
         /// @note Be sure that it is safe to de-reference the pointer.
         ///   The pointer is asserted to be non-null.
         constexpr T *operator->() const noexcept {
+            ASSERTF(_impl != nullptr, "Attempt to use expired pointer");
             auto ptr = _impl->getPointer();
             ASSERTF(ptr != nullptr, "Attempt to dereference null pointer");
             return ptr;
@@ -99,183 +105,139 @@ namespace hyper {
         /// @details Checks if the pointer can be safely de-referenced (is not null).
         /// @return True if the pointer is not null, or false if it is null.
         constexpr explicit operator bool() const noexcept {
-            return _impl->getPointer() != nullptr;
+            return _impl != nullptr && _impl->getPointer() != nullptr;
         }
 
-        /// @brief Assignment operator.
-        /// @details Overwrites the current pointer with a copy of another.
-        /// @param other Other pointer to overwrite the existing one with.
-        /// @return The current pointer.
-        SharedPointer &operator=(SharedPointer other) {
-            swap(other);
+        /// @brief Copy assignment operator.
+        /// @details Replaces the reference held by this pointer to a copy of another one.
+        /// @param other Other reference to copy.
+        /// @return Updated version of the existing pointer.
+        SharedPointer &operator=(SharedPointer const &other) {
+            expire();
+            _impl = other._impl;
+            _impl->increment();
             return *this;
         }
 
-        /// @brief Equality operator.
-        /// @details Checks if two shared pointers reference the same raw pointer.
-        /// @param other Other instance to compare against.
-        /// @return True if the instances reference the same pointer.
-        /// @return False if the instances reference different pointers.
-        /// @return True if both instances reference null.
-        /// @return False if only one instance references null.
-        constexpr bool operator==(const SharedPointer &other) const noexcept {
-            if (_impl == other._impl)
-                return true;
-            else
-                return _impl->getPointer() == other._impl->getPointer();
-        }
-
-        /// @brief Inequality operator.
-        /// @details Checks if two shared pointers don't reference the same raw pointer.
-        /// @param other Other instance to compare against.
-        /// @return True if the instances reference different pointers.
-        /// @return False if the instances reference the same pointer.
-        /// @return True if only one instance references null.
-        /// @return False if both instances reference null.
-        constexpr bool operator!=(const SharedPointer &other) const noexcept {
-            return !(this == other);
-        }
+    private:
+        ReferenceCounter<T> *_impl;
     };
 
-    /// @brief Smart pointer for arrays that can be shared throughout the application.
-    /// @details Smart pointer that tracks its references.
-    ///   The pointer will automatically be destroyed (and resources freed)
-    ///   when there are no more references to it.
-    ///   Instances of this class should have the only references to a raw pointer.
-    ///   This class is designed in such a way to attempt to prevent external references.
-    ///   This is a template specialization for pointers to arrays.
-    /// @tparam T Type the pointer references.
     template<typename T>
     class SharedPointer<T[]> {
-    private:
-        ReferenceCounter<T[]> *_impl;
-
     public:
+        /// @brief Default constructor.
+        /// @details Creates a new shared pointer that references null.
         constexpr SharedPointer() noexcept
                 : _impl(new ReferenceCounter<T[]>(nullptr)) {
             // ...
         }
 
-        /// @brief Default constructor.
-        /// @details Creates a new scoped pointer to a new array.
-        /// @param size Number of items in the array.
-        constexpr explicit SharedPointer(size_t size) noexcept
-                : _impl(new ReferenceCounter<T[]>(new T[size])) {
-            // ...
-        }
-
         /// @brief General constructor.
-        /// @details Creates a new shared pointer with an existing reference.
-        /// @param ptr Raw pointer to wrap.
-        constexpr explicit SharedPointer(T *&&ptr) noexcept
-                : _impl(new ReferenceCounter<T[]>(ptr)) {
+        /// @details Creates a new shared pointer that wraps an existing raw pointer.
+        /// @param rawPointer Raw pointer to wrap.
+        constexpr explicit SharedPointer(T *&&rawPointer) noexcept
+                : _impl(new ReferenceCounter<T[]>(rawPointer)) {
             // ...
         }
 
         /// @brief Copy constructor.
-        /// @details Creates a new shared pointer that references an existing one.
-        /// @param other Existing shared pointer.
+        /// @details Shares a reference to an existing pointer.
+        /// @param other Existing pointer to reference.
         SharedPointer(const SharedPointer &other) noexcept
                 : _impl(other._impl) {
             _impl->increment();
         }
 
-        /// @brief Move constructor.
-        /// @details Creates a new shared pointer from a temporary one.
-        /// @param other Temporary shared pointer.
-        SharedPointer(SharedPointer &&other) noexcept
-                : _impl(other._impl) {
-            other._impl = nullptr;
+        /// @brief Destructor.
+        /// @details Releases a reference to the underlying pointer.
+        ///   If there are no more references, then the pointer and any resources it holds are released.
+        ~SharedPointer() {
+            expire();
         }
 
-        /// @brief Destructor.
-        /// @details Releases the resources referenced by the shared pointer.
-        ///   The underlying pointer will not be released unless all other references have been released.
-        ~SharedPointer() noexcept {
-            if (_impl != nullptr) {
-                // Release reference counter if this instance is the last one using it.
-                if (!_impl->decrement())
+        /// @brief Force expiration of this reference.
+        /// @details Has the same effect as letting the shared pointer go out of scope.
+        ///   If there are no more references, then the pointer and any resources it holds are released.
+        void expire() noexcept {
+            if(_impl != nullptr) {
+                if(!_impl->decrement())
                     delete _impl;
                 _impl = nullptr;
             }
         }
 
-        /// @brief Swaps the contents of two Shared pointers.
-        /// @param other Shared pointer to swap with.
+        /// @brief Re-initializes the shared pointer.
+        /// @details Changes the raw pointer that the instance wraps.
+        ///   The existing pointer, if it has no other references, is freed.
+        /// @param rawPointer New pointer to wrap.
+        void reset(T *&&rawPointer = nullptr) noexcept {
+            expire();
+            _impl = new ReferenceCounter<T>(rawPointer);
+        }
+
+        /// @brief Swaps with another instance.
+        /// @details Exchanges the underlying pointer between two instances of the same type.
+        /// @param other Other pointer to swap with.
         void swap(SharedPointer &other) noexcept {
             auto temp = other._impl;
             other._impl = _impl;
             _impl = temp;
         }
 
-        /// @brief Subscript operator.
-        /// @details Provides access to a specified element in the array.
-        /// @param index Base-zero index of the element to access.
-        /// @return Element at the specified index.
         T &operator[](size_t index) noexcept {
-            auto arr = _impl->getPointer();
-            ASSERTF(arr != nullptr, "Attempt to dereference null pointer");
-            return arr[index];
+            ASSERTF(_impl != nullptr, "Attempt to use expired pointer");
+            auto ptr = _impl->getPointer();
+            ASSERTF(ptr != nullptr, "Attempt to dereference null pointer");
+            return ptr[index];
         }
 
-        /// @brief Subscript operator.
-        /// @details Retrieves a specified element in the array.
-        /// @param index Base-zero index of the element to access.
-        /// @return Element at the specified index.
         constexpr const T &operator[](size_t index) const noexcept {
-            auto arr = _impl->getPointer();
-            ASSERTF(arr != nullptr, "Attempt to dereference null pointer");
-            return arr[index];
+            ASSERTF(_impl != nullptr, "Attempt to use expired pointer");
+            auto ptr = _impl->getPointer();
+            ASSERTF(ptr != nullptr, "Attempt to dereference null pointer");
+            return ptr[index];
         }
 
         /// @brief Explicit bool cast.
         /// @details Checks if the pointer can be safely de-referenced (is not null).
         /// @return True if the pointer is not null, or false if it is null.
         constexpr explicit operator bool() const noexcept {
-            return _impl->getPointer() != nullptr;
+            return _impl != nullptr && _impl->getPointer() != nullptr;
         }
 
-        /// @brief Assignment operator.
-        /// @details Overwrites the current pointer with a copy of another.
-        /// @param other Other pointer to overwrite the existing one with.
-        /// @return The current pointer.
-        SharedPointer &operator=(SharedPointer other) {
-            swap(other);
+        /// @brief Copy assignment operator.
+        /// @details Replaces the reference held by this pointer to a copy of another one.
+        /// @param other Other reference to copy.
+        /// @return Updated version of the existing pointer.
+        SharedPointer &operator=(SharedPointer const &other) {
+            expire();
+            _impl = other._impl;
+            _impl->increment();
             return *this;
         }
 
-        /// @brief Equality operator.
-        /// @details Checks if two shared pointers reference the same raw pointer.
-        /// @param other Other instance to compare against.
-        /// @return True if the instances reference the same pointer.
-        /// @return False if the instances reference different pointers.
-        /// @return True if both instances reference null.
-        /// @return False if only one instance references null.
-        constexpr bool operator==(const SharedPointer &other) const noexcept {
-            if (_impl == other._impl)
-                return true;
-            else
-                return _impl->getPointer() == other._impl->getPointer();
-        }
-
-        /// @brief Inequality operator.
-        /// @details Checks if two shared pointers don't reference the same raw pointer.
-        /// @param other Other instance to compare against.
-        /// @return True if the instances reference different pointers.
-        /// @return False if the instances reference the same pointer.
-        /// @return True if only one instance references null.
-        /// @return False if both instances reference null.
-        constexpr bool operator!=(const SharedPointer &other) const noexcept {
-            return !(this == other);
-        }
+    private:
+        ReferenceCounter<T[]> *_impl;
     };
 
-    /// @brief Swaps the references of two shared pointers.
-    /// @param first First smart pointer to swap.
-    /// @param second Second smart pointer to swap.
-    template<typename T>
-    void swap(SharedPointer<T> &first, SharedPointer<T> &second) {
+    /// @brief Specialized implementation of swap for shared pointers.
+    /// @details Swaps the underlying references held by two pointers.
+    /// @param first First pointer to swap.
+    /// @param second Second pointer to swap.
+    /// @tparam T Type the smart pointers reference.
+    template<typename T, typename Subtype>
+    void swap(SharedPointer<T> first, SharedPointer<Subtype> second) noexcept {
         first.swap(second);
+    };
+
+    /// @brief Utility method for creating shared pointers.
+    /// @details Infers pointer types and creates a shared pointer for them.
+    /// @param ptr Raw pointer to wrap.
+    /// @return Newly constructed shared pointer.
+    template<typename T>
+    inline SharedPointer<T> createShared(T *&&ptr) {
+        return SharedPointer<T>(move(ptr));
     }
 }
 
