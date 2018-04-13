1b5d48056552d0a4a88a07bb523b83d32c80427b
Michael Miller <icy.arctic.fox@gmail.com>
2018-04-13 16:35:44 -0600

    Use shared pointer for functions and add some docs

--- include/hyper/Function.h
+++ include/hyper/Function.h
@@ -4,48 +4,87 @@
 #ifndef HYPER_FUNCTION_H
 #define HYPER_FUNCTION_H
 
-#include "UniquePointer.h"
+#include "SharedPointer.h"
 
 namespace hyper {
+    /// @brief Base function container class.
+    /// @details This class and type parameters allow for abbreviated syntax for functions.
+    /// @tparam Signature Function signature in the form: RETURN_TYPE(ARGUMENT_TYPES...)
     template<typename Signature>
     class Function {
         // ...
     };
 
+    /// @brief Specialized function container class.
+    /// @details This class is able to extract type parameters from the abbreviated syntax.
+    /// @tparam ReturnValue Type of the return value of the function.
+    /// @tparam Args Types for the arguments the function expects when called.
     template<typename ReturnValue, typename... Args>
     class Function<ReturnValue(Args...)> {
     private:
+        /// @brief Abstract base class for all callable types.
+        /// @details Provides type-erasure for callable types.
         class Callable {
         public:
+            /// @brief Destructor.
             virtual ~Callable() = default;
 
+            /// @brief Calls the instance.
+            /// @details Passes the arguments onto the callable instance and passed the return value back.
             virtual ReturnValue invoke(Args...) = 0;
         };
 
-        template<typename Signature>
+        /// @brief Concrete implementation of a callable type.
+        /// @details By using a template, all callable types that match the return type and parameters will work.
+        /// @tparam CallableType Type of the "thing" being called.
+        template<typename CallableType>
         class ConcreteCallable : public Callable {
         private:
-            Signature _func;
+            CallableType _func;
 
         public:
-            explicit ConcreteCallable(const Signature &func)
+            /// @brief Constructor.
+            /// @param func Callable instance.
+            explicit ConcreteCallable(const CallableType &func)
                     : _func(func) {
                 // ...
             }
 
+            /// @brief Destructor.
             ~ConcreteCallable() override = default;
 
+            /// @brief Calls the instance.
+            /// @details Passes the arguments onto the callable instance and passed the return value back.
+            /// @param args Arguments to pass to the callable instance.
+            /// @return Return value passed back from the callable instance.
             ReturnValue invoke(Args... args) {
                 return _func(args...);
             }
         };
 
-        UniquePointer<Callable> _callable;
+        SharedPointer<Callable> _callable;
 
     public:
-        template<typename Sig>
-        Function& operator=(Sig func) {
-            _callable = createUnique(new ConcreteCallable<Sig>(func));
+        /// @brief Default constructor.
+        /// @details Creates a function that references null.
+        Function() = default;
+
+        /// @brief Copy constructor.
+        /// @param other Existing function to copy from.
+        Function(const Function &other)
+                : _callable(other._callable) {
+            // ...
+        }
+
+        template<typename CallableType>
+        explicit Function(CallableType func)
+                : _callable(createShared(new ConcreteCallable<CallableType>(func))) {
+            // ...
+        }
+
+        template<typename CallableType>
+        Function& operator=(CallableType func) {
+            _callable = createShared(new ConcreteCallable<CallableType>(func));
             return *this;
         }
 
@@ -71,8 +110,12 @@ namespace hyper {
             return _callable != other._callable;
         }
 
+        /// @brief Functor operator.
+        /// @details Calls the underlying function.
+        /// @param args Arguments to pass to the function.
+        /// @return Value returned from the function.
         ReturnValue operator()(Args... args) const {
-            ASSERTF((bool)_callable, "Function pointer is null");
+            ASSERTF((bool)_callable, "Attempt to call null function");
             return _callable->invoke(args...);
         }
     };
